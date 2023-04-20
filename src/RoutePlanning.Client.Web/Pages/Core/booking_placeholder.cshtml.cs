e0a1d89d712493c307b1f616418afcb4675b4259
Anders Christoffersen <agch@netcompany.com>
2023-04-20 15:52:00 +0200

    Initial login page replaces the index, and login directs to a placeholder for the booking. Credentials are not currently checked.

---
+++ src/RoutePlanning.Client.Web/Pages/Core/booking_placeholder.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RoutePlanning.Client.Web.Pages.Core
+{
+    public class booking_placeholderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
