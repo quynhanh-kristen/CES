d34a96be44b943b4d2b18ad7abb53b911b1f9de4
npqa <npqa@netcompany.com>
2023-04-21 03:22:50 +1400

    First commit

---
+++ src/RoutePlanning.Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RoutePlanning.Infrastructure.Database;
+using Netcompany.Net.DomainDrivenDesign;
+using Netcompany.Net.UnitOfWork;
+using Netcompany.Net.UnitOfWork.AmbientTransactions;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace RoutePlanning.Infrastructure;
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddRoutePlanningInfrastructure(this IServiceCollection services)
+    {
+        var keepAliveConnection = new SqliteConnection("DataSource=:memory:");
+        keepAliveConnection.Open();
+        services.AddDbContext<RoutePlanningDatabaseContext>(builder =>
+        {
+            builder.UseSqlite(keepAliveConnection);
+            builder.ConfigureWarnings(x => x.Ignore(RelationalEventId.AmbientTransactionWarning));
+        });
+
+        services.AddDomainDrivenDesign(options => options.UseDbContext<RoutePlanningDatabaseContext>());
+        services.AddUnitOfWork(builder => builder.UseAmbientTransactions().With<RoutePlanningDatabaseContext>());
+
+        return services;
+    }
+}
